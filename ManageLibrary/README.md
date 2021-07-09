ae18996b7fdfa86c5371f5c9942764fd68193901
19020440 <you@example.com>
2021-07-09 11:30:06 +0700

    test-git

--- ManageLibrary/README.md
+++ ManageLibrary/README.md
@@ -1,42 +1,51 @@
 1. Các bước để chạy:
-- Lấy link git, git clone để tạo thư mục trong htdocs của xampp
-- cài composer link: getcomposer.org
-- git bash thư mục vừa clone hoặc chạy terminal của vs: chạy các lệnh:
-+ npm install
-+ composer install
-+ composer update
-(nếu bị lỗi 500) chạy :
-+ cp .env.example .env
-+ php artisan key:generate
-+ php artisan config:cache
+
+-   Lấy link git, git clone để tạo thư mục trong htdocs của xampp
+-   cài composer link: getcomposer.org
+-   git bash thư mục vừa clone hoặc chạy terminal của vs: chạy các lệnh:
+
+*   npm install
+*   composer install
+*   composer update
+    (nếu bị lỗi 500) chạy :
+*   cp .env.example .env
+*   php artisan key:generate
+*   php artisan config:cache
 
 -> php artisan serve
-- sau đó chạy lệnh: php artisan serve -> ra local của web mình
 
-2. quan trọng**** k được code ở nhánh develop, nếu lỡ thay đổi 1 số thứ ở develop thì chạy:
-+ git checkout . (quay lại develop như mới pull về)
+-   sau đó chạy lệnh: php artisan serve -> ra local của web mình
 
-3. tạo nhánh mới từ develop 
-+ git checkout develop (để ch sang nhánh develop)
-+ git pull (load, up lại nhành develop)
-+ git checkout -b tên-nhánh (tạo nhánh tên : tên-nhánh  giống develop)
--> code trên nhánh đó
+2. quan trọng\*\*\*\* k được code ở nhánh develop, nếu lỡ thay đổi 1 số thứ ở develop thì chạy:
+
+-   git checkout . (quay lại develop như mới pull về)
+
+3. tạo nhánh mới từ develop
+
+-   git checkout develop (để ch sang nhánh develop)
+-   git pull (load, up lại nhành develop)
+-   git checkout -b tên-nhánh (tạo nhánh tên : tên-nhánh giống develop)
+    -> code trên nhánh đó
 
 3. push code lên git, tạo request:
-+ git add .
-+ git commit - m "message"
-+ git push origin HEAD -f
--> sau đó lên git tạo request
+
+-   git add .
+-   git commit - m "message"
+-   git push origin HEAD -f
+    -> sau đó lên git tạo request
 
 4. sau mỗi lần tôi merge code của ae, thì ae về vscode, chuyển về nhánh develop, rồi pull lại code về -> code sẽ thay đổi như ae đã code:
-+ git checkout develop (chuyển sang nhánh develop)
-+ git pull origin HEAD (lấy code về)
--> sau đó ae lại tạo và chuyển sang nhánh mới để code rồi chạy:
-+ npm run watch (để load js)
-+ php artisan serve (để vào link local)
 
+-   git checkout develop (chuyển sang nhánh develop)
+-   git pull origin HEAD (lấy code về)
+    -> sau đó ae lại tạo và chuyển sang nhánh mới để code rồi chạy:
+-   npm run watch (để load js)
+-   php artisan serve (để vào link local)
 
-****Tôi mới thêm các phần ở component : (08/07)
-- các file .js ở component là các file ae sẽ viết react
-- ae vào thư mục routes, mở file web.php là sẽ thấy các đường dẫn để mở ở local
-- trong resources/views có các file .blade.php ae để ý các id để viết vào file react -> view đc
+\*\*\*\*Tôi mới thêm các phần ở component : (08/07)
+
+-   các file .js ở component là các file ae sẽ viết react
+-   ae vào thư mục routes, mở file web.php là sẽ thấy các đường dẫn để mở ở local
+-   trong resources/views có các file .blade.php ae để ý các id để viết vào file react -> view đc
+
+ao that su luon
--- ManageLibrary/resources/js/components/Example.js
+++ ManageLibrary/resources/js/components/Example.js
@@ -3,17 +3,223 @@ import ReactDOM from 'react-dom';
 
 function Example() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
+        <div className="grid grid2">
+        
+            {/* <div className="row1 addbook"> */}
+                {/* <input id="ao" className="aoao" type="checkbox"></input> */}
+                {/* <input id="ao1" className="aoao1" type="checkbox"></input> */}
+                <div className="pew1">
+                    
+                    
+                    <div className="toolbar">
+                        <div className="toolbar-namelibrary">
+                            <img src="https://library-management.com/uploads/satellite.png"/>
+                            <span className="shadows">Lib Mng</span>
+                        </div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="toolbar__profile">
+                            <div className="toolbar__profile-box">
+                                <img src="https://library-management.com/uploads/default.png"/>
+                                <div className="shadows">
+                                    <p >quan tri vien</p>
+                                    <p >quan tri vien cap cap</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="toolbar__catagory">
+                            <ul className="toolbar__catagory-list">
+                                <li className="toolbar__catagory-item">
+                                    <a href="" className="toolbar__catagory-link">
+                                        <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                            
+                                        <p className="shadows">Dashboard</p>
+                                    </a>
+                                </li>
+                                <li className="toolbar__catagory-item">
+                                    <a href="" className="toolbar__catagory-link">
+                                    <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                            
+                                        <p className="shadows">Receipts</p>
+                                    </a>
+                                </li>
+                                <li className="toolbar__catagory-item">
+                                    <a href="" className="toolbar__catagory-link">
+                                    <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                            
+                                        <p className="shadows">Manage notes</p>
+                                    </a>
+                                </li>
+                                <li className="toolbar__catagory-item">
+                                    <a href="" className="toolbar__catagory-link">
+                                    <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                           
+                                        <p className="shadows">Manage setting</p>
+                                    </a>
+                                </li>
+                                <li className="toolbar__catagory-item">
+                                    <a href="" className="toolbar__catagory-link">
+                                    <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                          
+                                        <p className="shadows">Manage user</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
+
+                
+                <div className="pew">
+                    <div className="maincontainer">
+                        <div className="grid maincontainer1">
+                            <div className="row1">
+                                <div className="col-12 maincontainer__thefirt">
+                                    <label for="ao">
+                                        <img src="https://img.icons8.com/dotty/50/000000/line-style.png"/>
+                                    </label>
+                                    
+                                    
+                                    <div className="maincontainer__profile">
+                                        <img src="https://img.icons8.com/material-outlined/24/000000/circled-chevron-down.png"/>
+                                        <div className="maincontainer__profile-box">
+                                            <ul className="maincontainer__profile-box-list">
+                                                <li className="maincontainer__profile-box-item"><img src="https://img.icons8.com/glyph-neue/25/000000/down-right.png"/>View Home</li>
+                                                <li className="maincontainer__profile-box-item"><img src="https://img.icons8.com/doodle/25/000000/user.png"/>Profile</li>
+                                                <li className="maincontainer__profile-box-item"><img src="https://img.icons8.com/doodle/25/000000/delete-sign.png"/>Logout</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                
+                                </div>
+
+                                <div className="col-12 maincontainer__thesecond">
+                                    <h1>Add boook</h1>
+                                    <span>Home/Page</span>
+                                </div>
+                            </div>
+
+                            <div className="maincontainer__thethird">
+                                <div className="row1 maincontainer__thethird-picturebox">
+                                    <div className="c-3 maincontainer__thethird-col3">
+                                        <div className="maincontainer__thethird-choosepicture">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8uZmGLYC1EDdxJKt6RRft6haNwkcIFGIohw&usqp=CAU"/>
+                                        </div>
+
+                                        <div className="maincontainer__thethird-choosepicture-button">
+                                            <div className="input-group input-group-lg maincontainer__thethird-choosepicture-button-box">
+                                                <div >
+                                                <span className="input-group-text" id="inputGroup-sizing-lg">Puslisher</span>
+                                                </div>
+                                                <input type="file"  aria-label="Large" aria-describedby="inputGroup-sizing-sm"> </input>
+                                            </div>
+                                        </div>
+                                    </div>
+        
+                                    <div className="c-8 maincontainer__thethird-col8">
+                                        <div className="row1">
+                                            <div className="c-6 maincontainer__thethird-ISBN">
+                                                <div className="input-group mb-3 maincontainer__thethird-ISBN-input">
+                                                    <div className="input-group-prepend">
+                                                    <span className="input-group-text" id="inputGroup-sizing-default">Default</span>
+                                                    </div>
+                                                    <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"> </input>
+                                                </div>
+                                                <button type="button" className="btn btn-dark">Search</button>
+                                            </div>
+                                            <div className="c-6 maincontainer__thethird-category">
+                                                <div className="input-group-prepend">
+                                                    <span className="input-group-text" id="inputGroup-sizing-default">Default</span>
+                                                </div>
+                                                <select className="maincontainer__thethird-category-select">
+                                                    <option selected>Select</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div className="row1">
+                                            <div className="c-12">
+                                                <div className="input-group input-group-lg">
+                                                    <div className="input-group-prepend">
+                                                    <span className="input-group-text" id="inputGroup-sizing-lg">Book Tiltle</span>
+                                                    </div>
+                                                    <input type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm"> </input>
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+
+                                        <div className="row1">
+                                            <div className="c-6">
+                                                <div className="input-group input-group-lg maincontainer__thethird-auther">
+                                                    <div >
+                                                    <span className="input-group-text" id="inputGroup-sizing-lg">Authers</span>
+                                                    </div>
+                                                    <input type="text"  aria-label="Large" aria-describedby="inputGroup-sizing-sm"></input>
+                                                </div>
+                                            </div>
+                                            <div className="c-6">
+                                                <div className="input-group input-group-lg maincontainer__thethird-puslish">
+                                                    <div >
+                                                    <span className="input-group-text" id="inputGroup-sizing-lg">Puslisher</span>
+                                                    </div>
+                                                    <input type="text"  aria-label="Large" aria-describedby="inputGroup-sizing-sm"></input>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="row1">
+                                            <div className="c-12">
+                                                <div className="input-group input-group-lg">
+                                                    <div className="input-group-prepend">
+                                                    <span className="input-group-text" id="inputGroup-sizing-lg">Book Tiltle</span>
+                                                    </div>
+                                                    <input type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm"></input>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="row1">
+                                            <div className="c-12">
+                                                <div className="input-group input-group-lg">
+                                                    <div className="input-group-prepend">
+                                                    <span className="input-group-text" id="inputGroup-sizing-lg">Book Tiltle</span>
+                                                    </div>
+                                                    <input type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm"></input>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="row1">
+                                            <div className="c-12">
+                                                <div className="form-group">
+                                                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                                                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="row1">
+                                            <div className="c-12">
+                                                <div className="input-group input-group-lg maincontainer__thethird-puslish">
+                                                    <div >
+                                                    <span className="input-group-text" id="inputGroup-sizing-lg">Puslisher</span>
+                                                    </div>
+                                                    <input type="text"  aria-label="Large" aria-describedby="inputGroup-sizing-sm"></input>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            {/* </div> */}
         </div>
+
     );
 }
 
