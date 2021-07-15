27e80412128f342b3c7ef6426d3c13ba42491828
19020440 <you@example.com>
2021-07-15 10:51:39 +0700

    thang1

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,3 +1,13 @@
-<div class="header">
-    Đây là nội dung của header, ở trang dashboard
+<div class="navbar1">  
+    <img src="https://img.icons8.com/dotty/30/000000/line-style.png"/> 
+    <div class="maincontainer__profile">
+        <img src="https://img.icons8.com/material-outlined/24/000000/circled-chevron-down.png"/>
+        <div class="maincontainer__profile-box">
+            <ul class="maincontainer__profile-box-list">
+                <li class="maincontainer__profile-box-item"><img src="https://img.icons8.com/glyph-neue/25/000000/down-right.png"/>View Home</li>
+                <li class="maincontainer__profile-box-item"><img src="https://img.icons8.com/doodle/25/000000/user.png"/>Profile</li>
+                <li class="maincontainer__profile-box-item"><img src="https://img.icons8.com/doodle/25/000000/delete-sign.png"/>Logout</li>
+            </ul>
+        </div>
+    </div>
 </div>
--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -1,3 +1,63 @@
-<div class="navbar">
-    Đây là nội dung của navbar, ở trang dashboard
-</div>
+<aside class="pew1">
+   
+            <div class="toolbar">
+                <div class="toolbar-namelibrary">
+                   
+                        <img src="https://library-management.com/uploads/satellite.png"/>
+                    
+                    <div>
+                        <span class="shadows">Lib Mng</span>
+                    </div>
+                    
+                </div>
+
+                <div class="toolbar__profile">
+                    <div class="toolbar__profile-box">
+                        <div class="toolbar__profile-box-img">
+                            <img src="https://library-management.com/uploads/default.png"/>
+                        </div>
+                        
+                        <div class="shadows">
+                            <p >Admin</p><br>
+                            <p >super admin</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="toolbar__catagory">
+                    <ul class="toolbar__catagory-list">
+                        <li class="toolbar__catagory-item">
+                            <a href="" class="toolbar__catagory-link">
+                                <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                            
+                                <p class="shadows">Dashboard</p>
+                            </a>
+                        </li>
+                        <li class="toolbar__catagory-item">
+                            <a href="" class="toolbar__catagory-link">
+                               <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                            
+                                <p class="shadows">Receipts</p>
+                            </a>
+                        </li>
+                        <li class="toolbar__catagory-item">
+                            <a href="" class="toolbar__catagory-link">
+                               <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                            
+                                <p class="shadows">Manage notes</p>
+                            </a>
+                        </li>
+                        <li class="toolbar__catagory-item">
+                            <a href="" class="toolbar__catagory-link">
+                               <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                           
+                                <p class="shadows">Manage setting</p>
+                            </a>
+                        </li>
+                        <li class="toolbar__catagory-item">
+                            <a href="" class="toolbar__catagory-link">
+                               <img src="https://img.icons8.com/dotty/80/000000/chess-clock.png"/>                                          
+                                <p class="shadows">Manage user</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+       
+    </aside>
