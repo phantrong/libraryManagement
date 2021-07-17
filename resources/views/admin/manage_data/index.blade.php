0a9bdee586f05a08321a471b6dcc73c9fcbb106f
19020440 <you@example.com>
2021-07-18 00:40:51 +0700

    edit-addbook-edit-dashboard

--- resources/views/admin/manage_data/index.blade.php
+++ resources/views/admin/manage_data/index.blade.php
@@ -90,6 +90,57 @@
                 </div>
 
             </section>
+
+            <section class="dashboard__content-dashboardMoney"> 
+                    <div class="row">
+                    <div class="col-xl-12 dashboard-money-content-header"> 
+                        <!-- <h1>Thống kê tiền sách theo các phân loại</h1> -->
+                        <h1 class="ribbon">
+                        <strong class="ribbon-content">Thống kê tiền sách theo các phân loại</strong>
+                    </h1>
+                    </div>
+                    
+                    <div class="col-xl-4 dashboard__content-dashboardMoney-search">
+                        <div class="dashboard-money-content-search">
+                            <div>
+                            <label class="input-group-text" for="typeofbook">Tìm theo</label>
+                            </div>
+                            <select class="custom-select custom-select1" id="typeofbook">
+                            <option selected>Chọn</option>
+                            <option value="1">Thể loại sách</option>
+                            <option value="2">Tác giả</option>
+                            <option value="3">Nhà xuất bản</option>
+                            </select>
+
+                            <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary btn-primary" type="button">Tìm</button>
+                            </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                
+
+                    
+                    
+                    
+
+                    <div class="col-xl-4 dashboard-money-content-total">
+                    <div class="col-xl-5">
+                        
+                    </div>
+                        <div class="col-xl-6">
+                            <span>Tổng tiền sách:</span>
+                            <span>100.000.000</span>
+                            <span>đ</span>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
         </div>
 @endsection
 
