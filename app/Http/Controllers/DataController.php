4e5f3c9cf76290d88ce1cadb650a1a2ef6cc47a1
phantrong001 <phantrong001@gmail.com>
2021-08-07 16:09:34 +0700

    deploy

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -18,7 +18,12 @@ class DataController extends Controller
         $totalUser = DB::table('users')->select(DB::raw('count(*) as totalUser'))->get();
         $totalOrder = DB::table('orders')->select(DB::raw('count(*) as totalOrder'))->get();
         $totalContact = DB::table('contacts')->select(DB::raw('count(*) as totalContact'))->get();
-        return view('admin.manage_data.index', compact('totalBook', 'totalUser', 'totalOrder', 'totalContact'));
+        return view('admin.manage_data.index', [
+            'totalBook' => $totalBook,
+            'totalUser' => $totalUser,
+            'totalOrder' => $totalOrder,
+            'totalContact' => $totalContact
+        ]);
     }
 
     /**
