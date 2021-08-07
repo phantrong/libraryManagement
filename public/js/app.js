5cc81610106c3e223b75f6565eeb1913730ff6d4
phantrong001 <phantrong001@gmail.com>
2021-08-07 17:37:40 +0700

    fix-navbar

--- public/js/app.js
+++ public/js/app.js
@@ -37402,13 +37402,13 @@ __webpack_require__(/*! ./clicked_navbar */ "./resources/js/clicked_navbar.js");
 
 var url = window.location.href;
 
-if (url.indexOf('admin') != -1) {
-  if (url.indexOf('book') != -1) {
-    if (url.indexOf('create') != -1) {
+if (url.indexOf('/admin') != -1) {
+  if (url.indexOf('/book') != -1) {
+    if (url.indexOf('/create') != -1) {
       $('#icon_managebook').addClass('hover');
       $('#page_managebook').addClass('activeHeight');
       $('#page_addbook').addClass('hover');
-    } else if (url.indexOf('edit') != -1) {
+    } else if (url.indexOf('/edit') != -1) {
       $('#icon_managebook').addClass('hover');
       $('#page_managebook').addClass('activeHeight');
       $('#page_editbook').addClass('hover');
@@ -37417,11 +37417,11 @@ if (url.indexOf('admin') != -1) {
       $('#page_managebook').addClass('activeHeight');
       $('#page_listbook').addClass('hover');
     }
-  } else if (url.indexOf('user') != -1) {
+  } else if (url.indexOf('/user') != -1) {
     $('#page_manageuser').addClass('hover');
-  } else if (url.indexOf('contact') != -1) {
+  } else if (url.indexOf('/contact') != -1) {
     $('#page_managecontact').addClass('hover');
-  } else if (url.indexOf('order') != -1) {
+  } else if (url.indexOf('/order') != -1) {
     $('#page_manageorder').addClass('hover');
   } else {
     $('#page_dashboard').addClass('hover');
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,13 +6,13 @@ require('./login_register');
 require('./clicked_navbar');
 
 var url = window.location.href;
-if (url.indexOf('admin') != -1) {
-    if (url.indexOf('book') != -1) {
-        if (url.indexOf('create') != -1) {
+if (url.indexOf('/admin') != -1) {
+    if (url.indexOf('/book') != -1) {
+        if (url.indexOf('/create') != -1) {
             $('#icon_managebook').addClass('hover');
             $('#page_managebook').addClass('activeHeight');
             $('#page_addbook').addClass('hover');
-        } else if (url.indexOf('edit') != -1) {
+        } else if (url.indexOf('/edit') != -1) {
             $('#icon_managebook').addClass('hover');
             $('#page_managebook').addClass('activeHeight');
             $('#page_editbook').addClass('hover');
@@ -21,11 +21,11 @@ if (url.indexOf('admin') != -1) {
             $('#page_managebook').addClass('activeHeight');
             $('#page_listbook').addClass('hover');
         }
-    } else if (url.indexOf('user') != -1) {
+    } else if (url.indexOf('/user') != -1) {
         $('#page_manageuser').addClass('hover');
-    } else if (url.indexOf('contact') != -1) {
+    } else if (url.indexOf('/contact') != -1) {
         $('#page_managecontact').addClass('hover');
-    } else if (url.indexOf('order') != -1) {
+    } else if (url.indexOf('/order') != -1) {
         $('#page_manageorder').addClass('hover');
     } else {
         $('#page_dashboard').addClass('hover');
